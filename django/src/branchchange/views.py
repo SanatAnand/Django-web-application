ef1b3b402b64a0d972c10f6d5b8986713a0315f1
SIDDHANT GARG <siddhant@nsl-62.cse.iitb.ac.in>
2015-10-26 23:28:43 +0530

    homepage createhomepage created

--- django/src/branchchange/views.py
+++ django/src/branchchange/views.py
@@ -2,13 +2,50 @@ from django.shortcuts import render
 
 from .forms import InitialFormSignup
 # Create your views here.
-def abc(request):
+def homeview(request):
 	topic= "BRANCH CHANGE"
 	if request.method == "POST":
 		print  request.POST
 	form=InitialFormSignup(request.POST or None)
+
 	context ={
 		"xyz": topic,
 		"form": form,
 	}
-	return render(request, "home.html",context)
+	return render(request, "homepage.html",context)
+
+def userloginview(request):
+	topic= "USER LOGIN" 
+	if request.method == "POST":
+		print  request.POST
+	form=InitialFormSignup(request.POST or None)
+
+	context ={
+		"xyz": topic,
+		"form": form,
+	}
+	return render(request, "userlogin.html",context)
+
+def adminloginview(request):
+	topic= "ADMIN LOGIN" 
+	if request.method == "POST":
+		print  request.POST
+	form=InitialFormSignup(request.POST or None)
+
+	context ={
+		"xyz": topic,
+		"form": form,
+	}
+	return render(request, "adminlogin.html",context)
+
+def registerview(request):
+	topic= "REGISTER AS A NEW USER" 
+	if request.method == "POST":
+		print  request.POST
+	form=InitialFormSignup(request.POST or None)
+
+	context ={
+		"xyz": topic,
+		"form": form,
+	}
+	return render(request, "register.html",context)
--- django/src/django1/urls.py
+++ django/src/django1/urls.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    url(r'^$', 'branchchange.views.abc', name='abc'),
+    url(r'^home$', 'branchchange.views.homeview', name='homeview'),
+    url(r'^home/userlogin$', 'branchchange.views.userloginview', name='userloginview'),
+    url(r'^home/adminlogin$', 'branchchange.views.adminloginview', name='adminloginview'),
+    url(r'^home/register$', 'branchchange.views.registerview', name='registerview'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
